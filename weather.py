3fd2eff7f7b9a2c40974f21ad984b5633bdd3e30
Raghvendra Sharma <raghvendrasharma1122@gmail.com>
2024-03-11 22:03:56 +0530

    fetched weather details using api

---
+++ weather.py
@@ -0,0 +1,11 @@
+import requests
+import json
+import win32com.client as wincom
+city = input("enter the name of the city\n")
+url =f"https://api.weatherapi.com/v1/current.json?key=c83ab6d59f4243c5bdb161140241103&q={city}"
+
+r=requests.get(url)
+# print(r.text)
+
+data = json.loads(r.text)
+print(data["current"]["temp_f"])
